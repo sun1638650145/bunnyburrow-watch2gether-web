00bf18786b1e39c00d043fd91e4545f56081a5e4
sun1638650145 <1638650145@qq.com>
2023-06-23 14:52:01 +0800

    Extract `MyMessage` & `OtherMessage` component.

--- src/components/message-list.jsx
+++ src/components/message-list.jsx
@@ -6,6 +6,48 @@ import {UserContext} from '../contexts.js';
 import '../styles/message-list.css';
 
 /**
+ * MyMessage组件, 用于展示当前用户发送的消息.
+ * @param {Object} messageObject - 消息对象.
+ * @returns {JSX.Element}
+ * @constructor
+ * @example
+ * <MyMessage messageObject={messageObject}/>
+ */
+function MyMessage({messageObject}) {
+    const name = messageObject.user.name;
+    const content = messageObject.content;
+
+    return (
+        <li className='my-message'>
+            <div className='content'>
+                {name}: {content}
+            </div>
+        </li>
+    );
+}
+
+/**
+ * OtherMessage组件, 用于展示其他用户发送的消息.
+ * @param {Object} messageObject - 消息对象.
+ * @returns {JSX.Element}
+ * @constructor
+ * @example
+ * <OtherMessage messageObject={messageObject}/>
+ */
+function OtherMessage({messageObject}) {
+    const name = messageObject.user.name;
+    const content = messageObject.content;
+
+    return (
+        <li className='other-message'>
+            <div className='content'>
+                {name}: {content}
+            </div>
+        </li>
+    );
+}
+
+/**
  * MessageList组件, 显示用户聊天内容.
  * @param {Object[]} chatList - 聊天内容列表.
  * @returns {JSX.Element}
@@ -19,23 +61,23 @@ export default function MessageList({chatList}) {
 
     return (
         <ol className='message-list'>
-            {chatList.map((inputContent, idx) =>
-                <li
-                    key={idx}
-                    className={
-                        inputContent.user.name === user.name ?
-                            'my-message' : 'other-message'
-                    }
-                >
-                    <div className='content'>
-                        {inputContent.user.name}: {inputContent.content}
-                    </div>
-                </li>
+            {chatList.map((messageObject, idx) =>
+                messageObject.user.name === user.name ? (
+                    <MyMessage key={idx} messageObject={messageObject}/>
+                ): (
+                    <OtherMessage key={idx} messageObject={messageObject}/>
+                )
             )}
         </ol>
     );
 }
 
+MyMessage.propTypes = {
+    messageObject: PropTypes.object.isRequired
+};
+OtherMessage.propTypes = {
+    messageObject: PropTypes.object.isRequired
+};
 MessageList.propTypes = {
-    chatList: PropTypes.arrayOf(PropTypes.object).isRequired,
+    chatList: PropTypes.arrayOf(PropTypes.object).isRequired
 };
