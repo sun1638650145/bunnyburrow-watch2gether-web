aefb25a1eb69deb2a830914cb992a06898395346
sun1638650145 <1638650145@qq.com>
2023-08-24 18:16:30 +0800

    Add `MessageList` tests.

---
+++ src/components/__tests__/message-list.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {describe, expect, test} from '@jest/globals';
+import '@testing-library/jest-dom/jest-globals';
+import {render, screen} from '@testing-library/react';
+
+import MessageList from '../message-list.jsx';
+
+describe('MessageList', () => {
+    test('正确渲染聊天内容', () => {
+        const chatList = [{
+            user: {
+                avatar: 'https://example.com/example.png',
+                name: 'Steve'
+            },
+            content: 'Hello!'
+        }];
+        render(<MessageList chatList={chatList}/>);
+
+        const otherMessage = screen.getByText('Hello!');
+
+        expect(otherMessage).toBeInTheDocument();
+    });
+});
