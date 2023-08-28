f7ba242cec5c327254acd9048daf6d99073f6d3b
sun1638650145 <1638650145@qq.com>
2023-08-28 14:24:43 +0800

    Add `VideoPlayerModal` tests.

---
+++ src/components/__tests__/video-player-modal.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {describe, expect, test} from '@jest/globals';
+import '@testing-library/jest-dom/jest-globals';
+import {render, screen} from '@testing-library/react';
+
+import VideoPlayerModal from '../video-player-modal.jsx';
+
+describe('VideoPlayerModal', () => {
+    test('显示模态框', () => {
+        render(<VideoPlayerModal isOpen={true} message='Hello, World!'/>);
+
+        const videoPlayerModal = screen.getByText('Hello, World!');
+
+        expect(videoPlayerModal).toBeInTheDocument();
+    });
+});
